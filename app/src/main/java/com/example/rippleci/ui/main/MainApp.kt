5f6d5d34c1b81cec335f8f773193df306e2ec83c
Jonathan <jona428@gmail.com>
2026-04-13 17:33:33 -0700

    Minor imports included

--- app/src/main/java/com/example/rippleci/ui/main/MainApp.kt
+++ app/src/main/java/com/example/rippleci/ui/main/MainApp.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -128,9 +129,10 @@ fun MainApp(onSignOut: () -> Unit) {
                 }
 
                 HelpfulLinksMenuButton(
-                    modifier = Modifier
-                        .align(Alignment.TopStart)
-                        .padding(start = 8.dp, top = 8.dp),
+                    modifier =
+                        Modifier
+                            .align(Alignment.TopStart)
+                            .padding(start = 8.dp, top = 8.dp),
                 )
             }
         }
